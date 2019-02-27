6e1e556cc06a737835fbc57c674a95a908ffe6c8
Bil. Müh. Halil <halilguler1994@gmail.com>
2019-02-27 14:59:55 +0300

    updated project

--- app/assets/scripts/modules/Person.js
+++
@@ -1,7 +0,0 @@
-function Person(fullname,favColor){
-    this.name = fullname;
-    this.favoriteColor = favColor;
-    this.greet = function(){
-        console.log("Helloi my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".")
-    }
-}
